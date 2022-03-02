9fe837f42582af0b8a505925e6fcf0ed53d0f089
ABIR LOUARD <79332920+AbirLOUARD@users.noreply.github.com>
2022-03-02 17:02:51 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import json
+import turtle
+import urllib.request
+import time
+import webbrowser
+import geocoder
+
+url = "http://api.open-notify.org/astros.json"
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+file = open("iss.txt", "w")
+file.write("There are currently " + str(result["number"]) + " astraunauts on the ISS: \n\n")
+
+people = result["people"]
+for p in people:
+    file.write(p['name'] + " - on board " + "\n")
+
+g = geocoder.ip("me")
+file.write("\n Your current lat / long is: " + str(g.latlng))
+file.close()
+webbrowser.open("iss.txt")
+
+
+screen = turtle.Screen()
+screen.setup(1200, 614)
+screen.setworldcoordinates(-180, -90, 180, 90)
+screen.bgpic("Map.gif")
+screen.register_shape("ISS.gif")
+iss = turtle.Turtle()
+iss.shape("ISS.gif")
+iss.setheading(45)
+iss.penup()
+
+
+while True :
+    url = "http://api.open-notify.org/iss-now.json"
+    response = urllib.request.urlopen(url)
+    result = json.loads(response.read())
+
+    location = result["iss_position"]
+    lat = location["latitude"]
+    lon = location["longitude"]
+
+    lat =float(lat)
+    lon = float(lon)
+    print("\n Latitude :" + str(lat), "Longitude :" + str(lon))
+
+
+    iss.goto(lon, lat)
+    time.sleep(1)
+
+
